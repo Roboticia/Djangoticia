bec6298fe44c15adecbf7f83e7a691b2e460393d
Julien JEHL <julien.jehl@roboticia.com>
2016-12-15 10:09:12 +0100

    wifi restarts

--- app1/urls.py
+++ app1/urls.py
@@ -9,7 +9,7 @@ urlpatterns = [
 	url(r'^settings/$', views.settings, name='configuration'),
 	url(r'^settings/wifi_add/$', views.wifi_add, name='wifi_add'),
 	url(r'^settings/wifi_suppr/$', views.wifi_suppr, name='wifi_suppr'),
-	url(r'^settings/wifi_restart/$', views.wifi_restart, name='wifi_suppr'),
+	url(r'^settings/wifi_restart/$', views.wifi_restart, name='wifi_restart'),
 	url(r'^juju/', views.juju, name='juju'),
 	url(r'^juju2/', views.juju2, name='juju2'),
 ]
