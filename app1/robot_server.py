e30aaff2d45c17836cdc0261b0f8b5998422de34
Julien JEHL <julien.jehl@roboticia.com>
2017-11-14 16:11:10 +0100

    new ip method

--- app1/robot_server.py
+++ app1/robot_server.py
@@ -16,44 +16,12 @@ def check_url(url):
     except :
         return False
 
-def find_local_ip(host=None):
-    # see here: http://stackoverflow.com/questions/166506/
-    try:
-        if host is None:
-            host = socket.gethostname()
-
-        if 'local' not in host:
-            host += '.local'
-
-        try:
-            ips = [ip for ip in socket.gethostbyname_ex(host)[2]
-                   if not ip.startswith('127.')]
-            if len(ips):
-                return ips[0]
-        except socket.gaierror:
-            logger.debug('socket gaierror with hostname {}'.format(host))
-            pass
-
-        # If the above method fails (depending on the system)
-        # Tries to ping google DNS instead (need a internet connexion)
-        try:
-            with closing(socket.socket()) as s:
-                s.settimeout(1)
-                s.connect(('8.8.8.8', 53))
-                return s.getsockname()[0]
-        except socket.timeout:
-            logger.debug('socket timeout')
-            pass
-
-    except IOError as e:
-        # network unreachable
-        # error no 10065 = WSAESERVERUNREACH Windows Network unreachable
-        if e.errno == errno.ENETUNREACH or e.errno == 10065:
-            logger.debug('network unreachable')
-            pass
-        else:
-            raise
-    return '127.0.0.1'
+def find_local_ip():
+    ip = (([ip for ip in socket.gethostbyname_ex(socket.gethostname()
+	)[2] if not ip.startswith("127.")] or [[(s.connect(("8.8.8.8", 53)
+	), s.getsockname()[0], s.close()) for s in [socket.socket(socket.AF_INET, socket.SOCK_DGRAM)]][0][1]]
+	) + ["no IP found"])[0]
+	return ip
     
 def robot_logs(robot):
     raw = ''
