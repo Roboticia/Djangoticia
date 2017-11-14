5ddabbbe22a44fd19549e301afa0c16791e2b2db
Julien JEHL <julien.jehl@roboticia.com>
2017-11-14 16:03:30 +0100

    right

--- change_right.sh
+++ change_right.sh
@@ -7,6 +7,7 @@ chown pi:www-data ./log/
 chown pi:www-data /var/www/
 chown pi:www-data ./my_python/
 chown root:www-data /
+chown root:www-data /etc/wpa_supplicant/wpa_supplicant.conf
 
 chmod 775 ../Djangoticia/
 chmod 775 ./log/
@@ -15,3 +16,4 @@ chmod 775 /var/www/
 chmod 664 ./db.sqlite3
 chmod 775 ./my_python/
 chmod 775 /
+chmod 660 /etc/wpa_supplicant/wpa_supplicant.conf
