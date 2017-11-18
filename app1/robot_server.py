2d75d4d1f5d5758ff0fab6228324a38430de81bb
Julien JEHL <julien.jehl@roboticia.com>
2017-11-18 08:51:55 +0100

    few find-local-ip()

--- app1/robot_server.py
+++ app1/robot_server.py
@@ -17,11 +17,11 @@ def check_url(url):
         return False
 
 def find_local_ip():
-    ip = (([ip for ip in socket.gethostbyname_ex(socket.gethostname()
-	)[2] if not ip.startswith("127.")] or [[(s.connect(("8.8.8.8", 53)
-	), s.getsockname()[0], s.close()) for s in [socket.socket(socket.AF_INET, socket.SOCK_DGRAM)]][0][1]]
-	) + ["no IP found"])[0]
-	return ip
+    ip = (([ip for ip in socket.gethostbyname_ex(socket.gethostname()	
+    )[2] if not ip.startswith("127.")] or [[(s.connect(("8.8.8.8", 53)
+    ), s.getsockname()[0], s.close()) for s in [socket.socket(socket.AF_INET, 
+    socket.SOCK_DGRAM)]][0][1]]	) + ["no IP found"])[0]
+    return ip
     
 def robot_logs(robot):
     raw = ''
